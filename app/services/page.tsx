2e439db2201c8fd29c7b49fc6f999741e1ab62e7
pushpakumar02 <pushpakumar2222000@gmail.com>
2025-09-08 13:45:51 -0500

    Fix service page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -9,25 +9,31 @@ export const metadata: Metadata = {
 export default function Services() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-medium">Services</h1>
+      <h1 className="mb-8 text-2xl font-medium">Service</h1>
       {/* Services Section */}
       <div className="mt-8">
         <h2 className="text-sm font-semibold text-gray-800 dark:text-gray-200 mb-2">Reviewer</h2>
 
         {/* Reviewer Roles */}
         <div className="mb-4">
-          <p className="text-sm text-gray-700 dark:text-gray-400">
-            <li>NeurIPS Conference Ethics Review, Datasets and Benchmarks Track Ethics Review; Workshops: SPIGM, AI4Science, ACA, AI4D3 — NeurIPS, 2025</li>
-          </p>
+          <ul className="list-disc pl-5">
+            <li className="text-sm text-gray-700 dark:text-gray-400">
+              NeurIPS Conference Ethics Review, Datasets and Benchmarks Track Ethics Review; Workshops: SPIGM, AI4Science, ACA, AI4D3 — NeurIPS, 2025
+            </li>
+          </ul>
         </div>
 
         {/* Artifact Evaluation / Committees */}
         <div className="mb-4">
           <h2 className="text-sm font-semibold text-gray-800 dark:text-gray-200 mb-2">Artifact Evaluation Committee</h2>
-          <p className="text-sm text-gray-700 dark:text-gray-400">
-            <li>EuroSys ’26 Artifact Evaluation Committee (Fall), 2025</li>
-            <li>FAST ’26 Artifact Evaluation Committee (Spring), 2025</li>
-          </p>
+          <ul className="list-disc pl-5">
+            <li className="text-sm text-gray-700 dark:text-gray-400">
+              EuroSys '26 Artifact Evaluation Committee (Fall), 2025
+            </li>
+            <li className="text-sm text-gray-700 dark:text-gray-400">
+              FAST '26 Artifact Evaluation Committee (Spring), 2025
+            </li>
+          </ul>
         </div>
       </div>
     </section>
