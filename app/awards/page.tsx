a54c026f27437bd7f0dc6f89e12807711ea90e3e
pushpakumar02 <pushpakumar2222000@gmail.com>
2026-03-06 17:21:47 -0600

    Update awards

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -21,6 +21,9 @@ export default function Awards() {
               <strong>Graduate Student Scholarly Research Fund</strong> · UCM Graduate Studies · Dec 2025
             </li>
             <li className="text-sm text-gray-700 dark:text-gray-400 mb-2">
+              <strong>AAAI-26 Student Scholarship & Volunteer Program</strong> · AAAI · Nov 2025
+            </li>
+            <li className="text-sm text-gray-700 dark:text-gray-400 mb-2">
               <strong>Graduate Student Scholarly Travel Fund</strong> · UCM Graduate Studies · Nov 2025
             </li>
             <li className="text-sm text-gray-700 dark:text-gray-400 mb-2">
